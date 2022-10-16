1192161c692f95c365381613addb896e1cf5ba44
Danny <93397389+Dcastro99@users.noreply.github.com>
2022-10-15 17:49:02 -0700

    lab 29

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,107 +1,133 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
+import React, { useState } from 'react';
 import './form.scss';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
-/**Yo figure it OUT!!!*/
+let apiStorage = [];
+
+
+
 export default function Form(props) {
   const [url, setUrl] = useState('');
   const [method, setMethod] = useState(null);
-  const [count, setCount] = useState('');
-  const [results, setResults] = useState([]);
-  // const buttonRef = useRef(null);
+  const [body, setBody] = useState(null);
+  const [show, setShow] = useState(false);
 
-  // const handleCLick = e => {
-  //   buttonRef.current.disabled = true;
-  //   setResults([]);
-  //   console.log('button clicked');
-  // }
 
-  // const handleReset = e => {
-  //   buttonRef.current.diabled = false;
-  // }
-
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     const response = await fetch(url, {
-  //       method: "POST",
-
-  //       body: JSON.stringify({ data: "value" }),
-  //     });
-  //     const json = await response.json();
-  //     console.log('okokok', json);
-
-  //     console.log('Ruuuuuu>>>::', response);
-  //   };
-  //   getData();
-  // });
-
-  useEffect(() => {
-    const getData = async () => {
-      // console.log('MYMETH::', method);
-      const data = {
-        method: method,
-        url: url,
-        body: JSON.stringify({ data: "value" })
-      }
-      const response = await axios(data);
-      console.log('RESSSS::', response);
-      setMethod(response.config.method)
-      setCount(response.data.count);
-      setResults(response.data.results)
-    }
-    getData();
-
-  })
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
 
   const handleSubmit = e => {
     e.preventDefault();
-    // handleCLick()
+
     const formData = {
-      count: count,
-      results: results,
+
       method: method,
       url: url,
+      body: body
     };
 
     props.handleApiCall(formData);
+    if (url !== null) {
+      apiStorage.push(url);
+    }
+
+    let histroyStr = JSON.stringify(apiStorage);
+    localStorage.setItem('results', histroyStr);
+
   }
+
+
 
   const onUrlChange = e => {
     e.preventDefault();
     setUrl(e.target.value)
-    console.log('POOP', e.target.value);
-  }
-  const onMethodChange = e => {
 
-    e.preventDefault();
-    setMethod(e.target.value)
-    console.log('METHOD', e.target.value);
+
+
+    // console.log('POOP', e.target.value);
   }
+
+  const onMethodChange = e => {
+    e.preventDefault();
+
+    setMethod(e.target.value)
+
+    // console.log('METHOD', e.target.value);
+  }
+
+  const onBodyChange = e => {
+    e.preventDefault();
+
+    setBody(e.target.value)
+
+    console.log('BODY', e.target.value);
+  }
+
+  const saved = JSON.parse(localStorage.getItem('results'));
+
+
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <label >
-          <span id='URL'>URL: </span>
-          <input name='url' type='text' onChange={(e) => onUrlChange(e)} />
-          <button id='form-button' type="submit">GO!</button>
-        </label>
-        <label className="methods">
-          <button onClick={(e) => onMethodChange(e)} value='GET' >GET</button >
-          <button id="post" onClick={(e) => onMethodChange(e)} value='POST'>POST</button>
-          <button id="put" onClick={(e) => onMethodChange(e)} value='PUT'>PUT</button >
-          <button id="delete" onClick={(e) => onMethodChange(e)} value='DELETE'>DELETE</button >
+      <div id='container'>
+        <form onSubmit={handleSubmit}>
+          <label >
+            <span id='URL'>URL: </span>
+            <input name='url' type='text' onChange={(e) => onUrlChange(e)} />
+            <button id='form-button' type="submit">GO!</button>
+          </label>
+          <div className="methods">
+            <button id="get" onClick={(e) => onMethodChange(e)} value='GET' >GET</button >
+            <button id="post" onClick={(e) => onMethodChange(e)} value='POST'>POST</button>
+            <button id="put" onClick={(e) => onMethodChange(e)} value='PUT'>PUT</button >
+            <button id="delete" onClick={(e) => onMethodChange(e)} value='DELETE'>DELETE</button >
+          </div>
+        </form>
+        <div id='modalMain'>
+          <div id="modalContainer">
+            <Button id='modalButton' variant="primary" onClick={handleShow}>
+              History
+            </Button>
+            <Modal classname='modal'
+              show={show}
+              onHide={handleClose}
+            >
+              <Modal.Header >
+                <Modal.Title id='modalTitle'>URL History</Modal.Title>
+              </Modal.Header>
+              {saved ? saved.map((x, key) =>
+              (
+                <Modal.Body>
+                  <h4 key={key}> - {x}</h4>
+                </Modal.Body>
+              )
+              ) : undefined}
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Close
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </div>
 
 
-        </label>
-        {/* <button onClick={handleReset}>Reset</button> */}
-      </form>
+
+
+          <div id='jsonLable'>
+            <textarea id='json' onChange={(e) => onBodyChange(e)} />
+          </div>
+
+        </div>
+      </div>
 
     </>
 
   )
 
 }
+
+
+
 
--- src/components/results/index.js
+++ src/components/results/index.js
@@ -6,7 +6,10 @@ export default function Results(props) {
   return (
     <section>
       <div className='pre-box'>
+        {/* <pre className='pre'>{props.data.headers ? JSON.stringify(props.data.headers, undefined, 2) : null}</pre> */}
         <pre className='pre'>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
+        {/* {console.log('PROPS!!', { props: props.data.data })} */}
+
       </div>
     </section>
   )
